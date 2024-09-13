068f58af2f26efe9fc672a105c7e97046e4b33b7
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-13 18:46:12 -0300

    feat: create header that hide on scroll down and shows on scrollup

--- src/app/(components)/header.tsx
+++ src/app/(components)/header.tsx
@@ -1,7 +1,59 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
 export default function Header() {
-    return <div className="w-full flex justify-between">
-        <div>logo</div>
-        <div>menu</div>
-        <div>cta</div>
-    </div>
+  const [isVisible, setIsVisible] = useState(true)
+  const [lastScrollY, setLastScrollY] = useState(0)
+
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      const scrollThreshold = 100
+
+      if (window.scrollY > scrollThreshold) {
+        if (window.scrollY > lastScrollY) {
+          setIsVisible(false)
+        } else {
+          setIsVisible(true)
+        }
+      } else {
+        setIsVisible(true)
+      }
+      setLastScrollY(window.scrollY)
+    }
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar)
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar)
+      }
+    }
+  }, [lastScrollY])
+
+  return (
+    <header
+      className={cn(
+        "fixed top-0 left-0 right-0 py-12 px-6 md:px-12  text-lg font-medium bg-neutral-50 transition-transform duration-300",
+        isVisible ? "translate-y-0" : "-translate-y-full"
+      )}
+    >
+      <nav className="flex flex-1 items-center justify-between">
+        <Link href="/">
+          <Image src="/bleu.png" alt="bleu's logo" width={100} height={100} />
+        </Link>
+        <div className="space-x-8">
+          <Link href="#/services">About</Link>
+          <Link href="#/services">Discover</Link>
+        </div>
+        <Link href="#/">Book a call</Link>
+      </nav>
+    </header>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,37 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Content from "./message.mdx";
-import Header from "./(components)/header";
-import Sections from "./(components)/sections";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
-    <main className="w-screen">
-      <Header/>
-      <Sections/>
+    <main className="pt-56">
+      <div className="flex flex-1 px-28">
+        <div className="space-y-9 flex flex-col flex-1">
+          {/* <Sections /> */}
+          <h1 id="first-section" className="text-5xl ">
+            Nurturing the potential of projects and individuals in Web3
+          </h1>
+          <p className="text-xl">
+            We specialize in creating customized software solutions to help
+            businesses thrive in today's digital landscape
+          </p>
+          <div className="flex gap-3">
+            <button
+              type="button"
+              className="-w p-4 rounded-md bg-secondary-200"
+            >
+              Build with us
+            </button>
+            <button type="button" className=" p-4 rounded-md bg-primary-200">
+              Join our team
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-52 h-52 bg-primary-600" />
+        </div>
+      </div>
+      <div className="mt-96">fake content</div>
+      <div className="mt-96">fake content</div>
+      <div className="mt-96">fake content</div>
+      <div className="mt-96">fake content</div>
+      <div className="mt-96">fake content</div>
     </main>
-  );
+  )
 }
