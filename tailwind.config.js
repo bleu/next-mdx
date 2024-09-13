d4b2e587ae0b549f8be347fdc3a6b45ced801dce
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-13 18:40:25 -0300

    chore: add tailwind colors structure, not the real design colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#e0f2f1",
+          100: "#b2dfdb",
+          200: "#80cbc4",
+          300: "#4db6ac",
+          400: "#26a69a",
+          500: "#009688",
+          600: "#00897b",
+          700: "#00796b",
+          800: "#00695c",
+          900: "#004d40"
+        },
+        secondary: {
+          50: "#fce4ec",
+          100: "#f8bbd0",
+          200: "#f48fb1",
+          300: "#f06292",
+          400: "#ec407a",
+          500: "#e91e63",
+          600: "#d81b60",
+          700: "#c2185b",
+          800: "#ad1457",
+          900: "#880e4f"
+        },
+        neutral: {
+          50: "#fafafa",
+          100: "#f5f5f5",
+          200: "#eeeeee",
+          300: "#e0e0e0",
+          400: "#bdbdbd",
+          500: "#9e9e9e",
+          600: "#757575",
+          700: "#616161",
+          800: "#424242",
+          900: "#212121"
+        }
+      }
+      // TODO: Add custom fonts
+      // fontFamily: {
+      //   sans: ["Inter var", "sans-serif"],
+      //   serif: ["Merriweather", "serif"]
+      // }
+    }
   },
-  plugins: [],
+  plugins: []
 }
