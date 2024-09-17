7295f72e611e8b2efe9a9795d94e0c9729484176
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-17 17:04:24 -0300

    chore: update first section text sizes and paddings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,22 +3,24 @@ export default function Home() {
     <main className="pt-56">
       <div className="flex flex-1 px-28">
         <div className="space-y-9 flex flex-col flex-1">
-          {/* <Sections /> */}
-          <h1 id="first-section" className="text-5xl ">
+          <h1 id="first-section" className="text-6xl ">
             Nurturing the potential of projects and individuals in Web3
           </h1>
-          <p className="text-xl">
+          <p className="text-2xl">
             We specialize in creating customized software solutions to help
             businesses thrive in today's digital landscape
           </p>
-          <div className="flex gap-3">
+          <div className="flex gap-3 flex-1">
             <button
               type="button"
-              className="-w p-4 rounded-md bg-secondary-200"
+              className="py-5 px-10 rounded-lg text-xl bg-secondary-400 text-black hover:bg-hover-secondary"
             >
               Build with us
             </button>
-            <button type="button" className=" p-4 rounded-md bg-primary-200">
+            <button
+              type="button"
+              className="py-5 px-10 rounded-lg text-xl bg-primary-500 text-white hover:bg-hover-primary"
+            >
               Join our team
             </button>
           </div>
@@ -27,11 +29,6 @@ export default function Home() {
           <div className="w-52 h-52 bg-primary-600" />
         </div>
       </div>
-      <div className="mt-96">fake content</div>
-      <div className="mt-96">fake content</div>
-      <div className="mt-96">fake content</div>
-      <div className="mt-96">fake content</div>
-      <div className="mt-96">fake content</div>
     </main>
   )
 }
