ec8bca44f5774e7e33e8bcff5f95edb31d0c3c29
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-17 20:43:27 -0300

    feat: create ourClient section

---
+++ src/components/ourClientButton.tsx
@@ -0,0 +1,32 @@
+import type React from "react"
+import Link from "next/link"
+
+interface OurClientButtonProps {
+  title: string
+  description: string
+  href: string
+}
+
+const OurClientButton: React.FC<OurClientButtonProps> = ({
+  title,
+  description,
+  href
+}) => {
+  return (
+    <button
+      className="group border border-black flex flex-col p-10 pt-5 pr-5 rounded-3xl"
+      type="button"
+    >
+      <div className="w-24 h-24 bg-black rounded-lg self-end" />
+      <h3 className="text-5xl">{title}</h3>
+      <p className="mt-16 text-justify invisible max-w-[75%] text-lg group-hover:visible">
+        {description}
+      </p>
+      <Link href={href} className="text-lg mt-6 invisible group-hover:visible">
+        Case study -&gt;
+      </Link>
+    </button>
+  )
+}
+
+export default OurClientButton
