d80748a8262c27d7f1bb52877f95ac2c8999aa9c
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2024-09-17 17:03:13 -0300

    chore: update header padding

--- src/app/(components)/header.tsx
+++ src/app/(components)/header.tsx
@@ -53,11 +53,11 @@ export default function Header() {
   return (
     <header
       className={cn(
-        "fixed top-0 left-0 right-0 py-12 px-6 md:px-12 text-lg font-medium bg-neutral-50 transition-transform duration-300",
+        "fixed top-0 left-0 right-0 px-6 md:px-12 text-lg font-medium bg-neutral-50 transition-transform duration-300",
         isVisible ? "translate-y-0" : "-translate-y-full"
       )}
     >
-      <nav className="flex flex-1 items-center justify-between">
+      <nav className="flex flex-1 items-center justify-between h-24 px-12">
         <Link href="/">
           <Image src="/bleu.png" alt="bleu's logo" width={100} height={100} />
         </Link>
